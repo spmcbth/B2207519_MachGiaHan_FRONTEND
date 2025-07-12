fb894675b3b0a3bd9b4bd17c5582f3cc50a3e1fc
spmcbth <machgiahan3624@gmail.com>
2025-07-13 01:56:12 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <script>
+import AppHeader from "@/components/AppHeader.vue";
+
 export default {
-}
+  components: {
+    AppHeader,
+  },
+};
 </script>
 
 <template>
-  <div class="page">
-    <h1>Hello, Vue.js!</h1>
+  <div id="app">
+    <AppHeader />
+    <div class="container mt-3">
+      <router-view />
+    </div>
   </div>
 </template>
 
 <style>
 .page {
-  max-width: 400px;
-  margin: auto;
-  text-align: center;
+  text-align: left;
+  max-width: 750px;
 }
 </style>
